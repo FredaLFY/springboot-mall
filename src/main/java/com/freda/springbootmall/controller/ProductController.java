af7603cea0a8444ee170dfda386463236be7adfe
Freda_Lin <fredalinfy@gmail.com>
2025-02-12 15:19:14 +0800

    商品功能 – 實作修改商品

--- src/main/java/com/freda/springbootmall/controller/ProductController.java
+++ src/main/java/com/freda/springbootmall/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.freda.springbootmall.controller;
 import com.freda.springbootmall.dto.ProductRequest;
 import com.freda.springbootmall.model.Product;
 import com.freda.springbootmall.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,11 +28,29 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity<Product> createProduct(@RequestBody @Validated ProductRequest productRequest) {
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest) {
         Integer productId = productService.createProduct(productRequest);
 
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+        // 檢查 product 是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // 修改商品的數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.ok(updateProduct);
     }
 }
