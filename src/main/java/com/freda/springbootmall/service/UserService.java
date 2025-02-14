6bbf13bf36b95fbcaaacfe1cae9e8e05e17b0268
Freda_Lin <fredalinfy@gmail.com>
2025-02-14 13:48:46 +0800

    帳號功能 – 登入

--- src/main/java/com/freda/springbootmall/service/UserService.java
+++ src/main/java/com/freda/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.freda.springbootmall.service;
 
+import com.freda.springbootmall.dto.UserLoginRequest;
 import com.freda.springbootmall.dto.UserRegisterRequest;
 import com.freda.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
